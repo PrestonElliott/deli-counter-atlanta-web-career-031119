27440dc512f1242cad39743ca1db71f2a9df43d2
Preston Elliott <preston.elliott24@gmail.com>
2019-03-04 22:02:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,14 @@
-def line 
+katz_deli = []
+
+def line(katz_deli) 
+  if katz_deli = 0 
+    "The line is currently empty."
+  else
+    "The line is currently: #{katz_deli}."
+    katz_deli += 1 
+  
+  
+  end
+end
   
 
