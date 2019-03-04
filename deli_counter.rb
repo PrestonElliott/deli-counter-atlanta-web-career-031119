9c8e6f7b1d73887a5c8443989a369bd9e8e9beba
Preston Elliott <preston.elliott24@gmail.com>
2019-03-04 22:03:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,7 @@
 katz_deli = []
 
 def line(katz_deli) 
-  if katz_deli = 0 
-    "The line is currently empty."
-  else
-    "The line is currently: #{katz_deli}."
-    katz_deli += 1 
   
-  
-  end
 end
   
 
